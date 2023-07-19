2331918a97a412fca228d9cfd7dce333434fc57d
Eshwar Muneshar <Emuneshar@gmail.com>
2023-07-19 16:54:39 -0400

    Added pen sprite code

--- Fireworks/Fireworks.py
+++ Fireworks/Fireworks.py
@@ -6,3 +6,10 @@ screen.tracer(False)
 screen.bgcolor("#161445")
 
 colors = ["red", "green", "orange", "yellow", "purple", "cyan"]
+
+# Create Pen
+pen = turtle.Turtle()
+pen.color("white")
+pen.hideturtle()
+pen.setheading(90)
+
